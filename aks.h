0409ff7f3b28fa2a9a47c99d87f9f2377aa0607f
NFeingold <41585313+NFeingold@users.noreply.github.com>
2018-07-30 09:21:43 -0700

    Add files via upload
    Update to leave word formatting

--- aks.h
+++ aks.h
@@ -18,6 +18,5 @@ Send, az group create -n %resource% --location eastus && az iot hub create -n %h
 InputBox, secret, Enter Secret, , 640, 480
 if secret =
     Exit
-Send, kubectl create secret generic my-secrets --from-literal=hub0-cs='%secret%' && kubectl create serviceaccount --namespace kube-system tiller && kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller && kubectl patch deploy --namespace kube-system tiller-deploy -p '{{}"spec":{{}"template":{{}"spec":{{}"serviceAccount":"tiller"{}}{}}{}}{}}' && helm init --service-account tiller -upgrade && helm repo update && cd iot-edge-virtual-kubelet-provider/src/charts/iot-edge-connector/ && helm install -n hub0 --set rbac.install=true . {Enter}
-
-return  
+Send, kubectl create secret generic my-secrets --from-literal=hub0-cs=’l’ && helm init && kubectl create serviceaccount --namespace kube-system tiller && kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller && kubectl patch deploy --namespace kube-system tiller-deploy -p ‘{“spec”:{“template”:{“spec”:{“serviceAccount”:”tiller”}}}}’ && helm init --service-account tiller --upgrade && helm repo update && cd iot-edge-virtual-kubelet-provider/src/charts/iot-edge-connector/ && helm install -n hub0 --set rbac.install=true . 
+return 
--- secrets.h
+++ secrets.h
@@ -3,6 +3,6 @@
 InputBox, secret, Enter Secret, , 640, 480
 if secret =
     Exit
-Send, kubectl create secret generic my-secrets --from-literal=hub0-cs='%secret%' && kubectl create serviceaccount --namespace kube-system tiller && kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller && kubectl patch deploy --namespace kube-system tiller-deploy -p '{{}"spec":{{}"template":{{}"spec":{{}"serviceAccount":"tiller"{}}{}}{}}{}}' && helm init --service-account tiller -upgrade && helm repo update && cd iot-edge-virtual-kubelet-provider/src/charts/iot-edge-connector/ && helm install -n hub0 --set rbac.install=true . {Enter}
-
+Send, kubectl create secret generic my-secrets --from-literal=hub0-cs=’%secret%’ && kubectl --namespace kube-system create serviceaccount tiller && kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller && helm init --service-account tiller && cd iot-edge-virtual-kubelet-provider/src/charts/iot-edge-connector/ && helm install -n hub0 --set rbac.install=true . && cd {Enter}
 return 
+
